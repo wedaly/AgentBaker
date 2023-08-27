9c13eda50a93fc1ebe1192876dd93b35a49c1b2b
Will Daly <widaly@microsoft.com>
2023-08-27 15:33:30 -0700

    and preload flexvol

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -297,6 +297,7 @@ fi
 
 # WIDALY DEBUG: caching experiment
 echo "WIDALY DEBUG: preloading cache for calico binaries, start $(date)"
+find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "flexvol*" -type f | xargs wc -l
 find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "bandwidth" -type f | xargs wc -l
 find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "calico*" -type f | xargs wc -l
 find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "flannel" -type f | xargs wc -l
@@ -308,6 +309,7 @@ find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "tuning" 
 echo "WIDALY DEBUG: finished preloading cache for calico binaries, end $(date)"
 
 echo "WIDALY DEBUG: preloading cache for calico binaries, start $(date) take two"
+find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "flexvol*" -type f | xargs wc -l
 find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "bandwidth" -type f | xargs wc -l
 find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "calico*" -type f | xargs wc -l
 find /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/ -name "flannel" -type f | xargs wc -l
