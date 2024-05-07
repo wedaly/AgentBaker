5a9d5b76c9761fa30182abf34839705f34b3d7cf
Will Daly <widaly@microsoft.com>
2024-05-07 13:44:26 -0700

    feat: Windows CSE invokes Windows next-gen networking func

--- parts/windows/kuberneteswindowssetup.ps1
+++ parts/windows/kuberneteswindowssetup.ps1
@@ -190,6 +190,10 @@ $global:WindowsCalicoPackageURL = "{{GetVariable "windowsCalicoPackageURL" }}";
 ## GPU install
 $global:ConfigGPUDriverIfNeeded = [System.Convert]::ToBoolean("{{GetVariable "configGPUDriverIfNeeded" }}");
 
+# Windows next-gen networking (Windows eBPF)
+$global:EnableNextGenNetworking = [System.Convert]::ToBoolean("{{GetVariable "windowsEnableNextGenNetworking" }}");
+$global:NextGenNetworkingURLBase = "{{GetVariable "windowsNextGenNetworkingURLBase"}}"
+
 # GMSA
 $global:WindowsGmsaPackageUrl = "{{GetVariable "windowsGmsaPackageUrl" }}";
 
@@ -268,6 +272,7 @@ try
     . c:\AzureData\windows\kubeletfunc.ps1
     . c:\AzureData\windows\kubernetesfunc.ps1
     . c:\AzureData\windows\nvidiagpudriverfunc.ps1
+    . c:\AzureData\windows\nextgennetworking.ps1
 
     # Install OpenSSH if SSH enabled
     $sshEnabled = [System.Convert]::ToBoolean("{{ WindowsSSHEnabled }}")
@@ -451,7 +456,9 @@ try
     }
 
     Start-InstallGPUDriver -EnableInstall $global:ConfigGPUDriverIfNeeded -GpuDriverURL $global:GpuDriverURL
-    
+
+    Start-InstallWindowsNextGenNetworkingIfNeeded -EnableInstall $global:EnableNextGenNetworking -URLBase $global:NextGenNetworkingURLBase
+
     if (Test-Path $CacheDir)
     {
         Write-Log "Removing aks cache directory"
